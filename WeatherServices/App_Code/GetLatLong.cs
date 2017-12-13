b2ec8c8eba0fd9e9d750684d6cbb0cfea137e149
Inderjit Singh Dhillon <34362991+singhinderjit@users.noreply.github.com>
2017-12-13 12:31:38 +0530

    Add files via upload

---
+++ WeatherServices/App_Code/GetLatLong.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Activation;
+using Newtonsoft.Json;
+
+/// <summary>
+/// Summary description for CityService
+/// </summary>
+public class GetLatLong : IGetLatLong
+{
+    public LatLong getlatlong(string stateOrCity)
+    {
+        string output;
+        LatLong lst = new LatLong();
+        using (WebClient client = new WebClient())
+        {
+            string input = "http://autocomplete.wunderground.com/aq?query=" + stateOrCity;
+            output = client.DownloadString(input);
+            lst = JsonConvert.DeserializeObject<LatLong>(output);
+            return lst;
+        }
+
+    }
+}
---
+++ WeatherServices/App_Code/IIArea.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+/// <summary>
+/// Summary description for iAreaSquare
+/// </summary>
+
+[ServiceContract]
+public interface IIArea
+{
+    [OperationContract]
+    double AreaofSquare(double sides);
+
+    [OperationContract]
+    double AreaofRectangle(double length, double breadth);
+
+    [OperationContract]
+    double GetSpeed(double distance, double time);
+
+    [OperationContract]
+    double GetDistance(double speed, double time);
+}
