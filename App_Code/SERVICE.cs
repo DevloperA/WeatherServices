bddbc1b0177390df1291e305969b9456d916bf29
DevolperB <singhharwinder@seasiainfotech.com>
2018-01-12 16:57:31 +0530

    Commit

---
+++ App_Code/SERVICE.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for SERVICE
+/// </summary>
+public class SERVICE
+{
+
+    public int Subtraction(int value1, int value2)
+    {
+        return value1 - value2;
+    }
+    
+}
