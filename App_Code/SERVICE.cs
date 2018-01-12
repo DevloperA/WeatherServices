3265d93e7fbb89dd89670b169f7a37e9fa580f4c
DevolperB <singhharwinder@seasiainfotech.com>
2018-01-12 18:20:01 +0530

    Commit

--- App_Code/SERVICE.cs
+++ App_Code/SERVICE.cs
@@ -13,5 +13,35 @@ public class SERVICE
     {
         return value1 - value2;
     }
-    
+    public int Subtraction1(int value1, int value2)
+    {
+        return value1 - value2;
+    }
+
+
+    public int Subtraction2(int value1, int value2)
+    {
+        return value1 - value2;
+    }
+
+    public int Subtraction3(int value1, int value2)
+    {
+        return value1 - value2;
+    }
+
+    public int Subtraction4(int value1, int value2)
+    {
+        return value1 - value2;
+    }
+
+    public int Subtraction5(int value1, int value2)
+    {
+        return value1 - value2;
+    }
+
+    public int Subtraction6(int value1, int value2)
+    {
+        return value1 - value2;
+    }
+
 }
