552dad4bc84bcffe508049218a5358436ea57f52
DevolperB <singhharwinder@seasiainfotech.com>
2018-01-12 18:36:59 +0530

    Commit

---
+++ App_Code/ClassFour.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for ClassFour
+/// </summary>
+public class ClassFour
+{
+    public int Addition(int val1, int val2)
+    {
+        return val1 + val2;
+    }
+    public int Execute()
+    {
+        int val1 = 1;
+        int val2 = 2;
+        ClassTwo obj = new ClassTwo();
+        return obj.Addition(val1, val2);
+
+    }
+    public int Addition1(int val1, int val2)
+    {
+        return val1 + val2;
+    }
+    public int Execute1()
+    {
+        int val1 = 1;
+        int val2 = 2;
+        ClassTwo obj = new ClassTwo();
+        return obj.Addition(val1, val2);
+
+    }
+    public int Addition2(int val1, int val2)
+    {
+        return val1 + val2;
+    }
+    public int Execute2()
+    {
+        int val1 = 1;
+        int val2 = 2;
+        ClassTwo obj = new ClassTwo();
+        return obj.Addition(val1, val2);
+
+    }
+    public int Addition3(int val1, int val2)
+    {
+        return val1 + val2;
+    }
+    public int Execute3()
+    {
+        int val1 = 1;
+        int val2 = 2;
+        ClassTwo obj = new ClassTwo();
+        return obj.Addition(val1, val2);
+
+    }
+    public int Addition4(int val1, int val2)
+    {
+        return val1 + val2;
+    }
+    public int Execute4()
+    {
+        int val1 = 1;
+        int val2 = 2;
+        ClassTwo obj = new ClassTwo();
+        return obj.Addition(val1, val2);
+
+    }
+
+}
