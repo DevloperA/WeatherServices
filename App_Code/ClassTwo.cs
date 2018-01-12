37fa3f906d55e24d37054fe50de9d7308ed062bb
DevolperB <singhharwinder@seasiainfotech.com>
2018-01-12 18:29:52 +0530

    Commit

---
+++ App_Code/ClassTwo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public class ClassTwo
+{
+    /// <summary>
+    /// Addition
+    /// </summary>
+    /// <param name="val1"></param>
+    /// <param name="val2"></param>
+    /// <returns></returns>
+    public int Addition(int val1, int val2)
+    {
+        return val1 + val2;
+    }
+    public int Execute()
+    {
+        int val1 = 1;
+        int val2 = 2;
+        ClassTwo obj = new ClassTwo();
+        return obj.Addition(val1, val2);
+
+    }
+}
