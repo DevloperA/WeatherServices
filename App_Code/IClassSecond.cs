9a2ba7b6ca03f91a8156a84ae37b66ca93a256ef
DevolperB <singhharwinder@seasiainfotech.com>
2018-01-15 17:33:47 +0530

    Fifth Commit

---
+++ App_Code/IClassSecond.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+[ServiceContract]
+public interface IClassSecond
+{
+    [OperationContract]
+    int Mul();
+}
