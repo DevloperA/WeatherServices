5014b741c3befd6c9807adc82081cc5f8b32e051
DevolperB <singhharwinder@seasiainfotech.com>
2018-01-15 17:25:55 +0530

    Fourth Commit

---
+++ App_Code/ClassSecond.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public class ClassSecond
+{
+   public int Mul()
+    {
+        int a = 20 * 2;
+        return a;
+    }
+}
