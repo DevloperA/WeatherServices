6f51737d0c8bcc645a174a758d02728dacfadc1e
DevolperB <singhharwinder@seasiainfotech.com>
2018-01-15 18:08:59 +0530

    8th Commit

---
+++ App_Code/ClassThird.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public class ClassThird
+{
+    public int Balance(int amount, int bill)
+    {
+        return amount - bill;
+    }
+}
