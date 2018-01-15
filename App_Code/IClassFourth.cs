540c5d265d5eda694a6cd203e4769178a4e2f618
DevolperB <singhharwinder@seasiainfotech.com>
2018-01-15 19:12:12 +0530

    13th Commit

---
+++ App_Code/IClassFourth.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+
+[ServiceContract]
+public interface IClassFourth
+{
+    [OperationContract]
+    int Addition();
+}
