a02fc79109d92c1acc24a790f5963d1e6658bf44
DevolperB <singhharwinder@seasiainfotech.com>
2018-01-15 19:06:09 +0530

    12th Commit

---
+++ App_Code/ClassFourth.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+public class ClassFourth
+{
+   public int Addition()
+    {
+        return 20 + 10;
+    }
+}
