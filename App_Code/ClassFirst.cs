b616c4d3e82bfc486f7999452d53138bf0b1e541
DevolperB <singhharwinder@seasiainfotech.com>
2018-01-15 16:36:01 +0530

    FIrst Commit

---
+++ App_Code/ClassFirst.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for ClassFirst
+/// </summary>
+public class ClassFirst
+{
+   public void Sum()
+    {
+        int a = 20 + 10;
+    }
+}
